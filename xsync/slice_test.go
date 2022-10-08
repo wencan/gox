b99b5dab569ef0fb8b3da4667aa82a8f5a2b5bcb
wencan <i@wencan.org>
2022-10-08 12:11:22 +0800

    add unittest

--- xsync/slice_test.go
+++ xsync/slice_test.go
@@ -175,3 +175,50 @@ func TestSlice_ConcurrentlyUpdateAt(t *testing.T) {
 		}
 	}
 }
+
+func TestSlice_ConcurrentlyAppendAndUpdateAt(t *testing.T) {
+	var slice Slice
+
+	var wg sync.WaitGroup
+	wg.Add(1000)
+	letGo := make(chan int)
+	indexChann := make(chan int, 1000000)
+	for i := 0; i < 1000; i++ {
+		go func() {
+			defer wg.Done()
+
+			<-letGo
+
+			for j := 0; j < 1000; j++ {
+				// 随便塞个数据
+				index := slice.Append(j)
+
+				// 先自己更新一次
+				slice.UpdateAt(index, index*2)
+
+				// 请其它goroutine更新一次
+				indexChann <- index
+
+				// 更新其它的goroutine的数据一次
+				index = <-indexChann
+				num, _ := slice.Load(index).(int)
+				slice.UpdateAt(index, num*5)
+			}
+		}()
+	}
+
+	time.Sleep(time.Millisecond * 200)
+	close(letGo)
+
+	wg.Wait()
+
+	// 检查
+	slice.Range(func(index int, p interface{}) (stopIteration bool) {
+		num, _ := p.(int)
+		if num != index*10 {
+			t.Fatalf("want %d, got %d", index*10, num)
+			return true
+		}
+		return false
+	})
+}
