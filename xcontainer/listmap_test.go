de6df4efa0efeb82258fc71d741cb1414216962e
wencan <i@wencan.org>
2022-10-09 15:27:49 +0800

    add xcontainer/ListMap

---
+++ xcontainer/listmap_test.go
@@ -0,0 +1,62 @@
+package xcontainer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestListMap_PushBack(t *testing.T) {
+	m := NewListMap()
+
+	t1 := time.Now().Nanosecond()
+	t2 := time.Now().Nanosecond()
+	m.PushBack(1, t1)
+	m.PushFront(2, t2)
+
+	v, _ := m.Get(1)
+	assert.Equal(t, t1, v)
+
+	v1, _ := m.Back()
+	v2, _ := m.Front()
+	assert.Equal(t, t1, v1)
+	assert.Equal(t, t2, v2)
+	assert.Equal(t, 2, m.Len())
+}
+
+func TestListMap_PopFront(t *testing.T) {
+	m := NewListMap()
+
+	t1 := time.Now().Nanosecond()
+	t2 := time.Now().Nanosecond()
+	m.PushBack(1, t1)
+	m.PushFront(2, t2)
+
+	v, _ := m.PopFront()
+	assert.Equal(t, t2, v)
+
+	v, _ = m.Front()
+	assert.Equal(t, t1, v)
+	assert.Equal(t, 1, m.Len())
+
+	m.PopFront()
+	m.PopFront()
+	assert.Equal(t, 0, m.Len())
+}
+
+func TestListMap_MoveToFront(t *testing.T) {
+	m := NewListMap()
+
+	t1 := time.Now().Nanosecond()
+	t2 := time.Now().Nanosecond()
+	m.PushBack(1, t1)
+	m.PushFront(2, t2)
+
+	m.MoveToFront(1)
+
+	v1, _ := m.Back()
+	v2, _ := m.Front()
+	assert.Equal(t, t2, v1)
+	assert.Equal(t, t1, v2)
+}
