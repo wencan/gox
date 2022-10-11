7b61d26de67ce76d4c4edd231b57c9b60d29fd52
wencan <i@wencan.org>
2022-10-11 12:00:23 +0800

    优化ListMap

--- xcontainer/listmap.go
+++ xcontainer/listmap.go
@@ -2,12 +2,17 @@ package xcontainer
 
 import (
 	"container/list"
+	"sync"
 )
 
 type listEntry struct {
 	key   interface{}
 	value interface{}
 }
+
+var listEntryPool = sync.Pool{New: func() interface{} {
+	return &listEntry{}
+}}
 
 // ListMap 同时具备List和Map的特性。
 // 来自：https://github.com/wencan/cachex。
@@ -39,7 +44,11 @@ func (m *ListMap) Pop(key interface{}) (value interface{}, ok bool) {
 		m.sequence.Remove(elem)
 
 		entry := elem.Value.(*listEntry)
-		return entry.value, true
+		value := entry.value
+
+		listEntryPool.Put(entry)
+
+		return value, true
 	}
 	return nil, false
 }
@@ -73,8 +82,11 @@ func (m *ListMap) PopFront() (value interface{}, ok bool) {
 
 		m.sequence.Remove(elem)
 		delete(m.mapping, entry.key)
+		value = entry.value
 
-		return entry.value, true
+		listEntryPool.Put(entry)
+
+		return value, true
 	}
 	return nil, false
 }
@@ -86,27 +98,28 @@ func (m *ListMap) PopBack() (value interface{}, ok bool) {
 
 		m.sequence.Remove(elem)
 		delete(m.mapping, entry.key)
+		value = entry.value
 
-		return entry.value, true
+		listEntryPool.Put(entry)
+
+		return value, true
 	}
 	return nil, false
 }
 
 func (m *ListMap) PushFront(key interface{}, value interface{}) {
-	entry := &listEntry{
-		key:   key,
-		value: value,
-	}
+	entry := listEntryPool.Get().(*listEntry)
+	entry.key = key
+	entry.value = value
 
 	elem := m.sequence.PushFront(entry)
 	m.mapping[key] = elem
 }
 
 func (m *ListMap) PushBack(key interface{}, value interface{}) {
-	entry := &listEntry{
-		key:   key,
-		value: value,
-	}
+	entry := listEntryPool.Get().(*listEntry)
+	entry.key = key
+	entry.value = value
 
 	elem := m.sequence.PushBack(entry)
 	m.mapping[key] = elem
@@ -127,6 +140,10 @@ func (m *ListMap) MoveToBack(key interface{}) {
 }
 
 func (m *ListMap) Clear() {
+	for _, elem := range m.mapping {
+		listEntryPool.Put(elem.Value)
+	}
+
 	m.mapping = make(map[interface{}]*list.Element)
 	m.sequence = list.New()
 }
