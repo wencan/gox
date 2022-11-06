c53237d6160d2463833d7c8800c2854960b2ed66
wencan <i@wencan.org>
2022-11-06 12:13:17 +0800

    add ParallelLimit

--- async/parallel.go
+++ async/parallel.go
@@ -1,29 +1,64 @@
 package async
 
-import "sync"
+import (
+	"sync"
+	"sync/atomic"
+)
 
 // Parallel 并行执行一组函数，等待执行完成。
-// 如果其中一个或多个函数返回错误，优先选择前面函数的错误返回。
-// 如果其中一个或多个函数panic，优先选择前面函数的recover()的非nil结果再次panic。
+// 如果其中一个或多个函数返回错误，未执行的函数不再执行，并优先选择前面函数的错误返回。
+// 如果其中一个或多个函数panic，未执行的函数不再执行，并优先选择前面函数的recover()的非nil结果再次panic。
 func Parallel(funcs ...func() error) error {
+	return ParallelLimit(len(funcs), funcs...)
+}
+
+// ParallelLimit 并行执行一组函数，等待执行完成。
+// 每次调用最多只执行limit路并发执行。前面的函数优先执行。
+// 如果其中一个或多个函数返回错误，未执行的函数不再执行，并优先选择前面函数的错误返回。
+// 如果其中一个或多个函数panic，未执行的函数不再执行，并优先选择前面函数的recover()的非nil结果再次panic。
+func ParallelLimit(limit int, funcs ...func() error) error {
 	var errs = make([]error, len(funcs))
 	var recovereds = make([]interface{}, len(funcs))
-
 	var wg sync.WaitGroup
-	wg.Add(len(funcs))
-	for i, f := range funcs {
-		i, f := i, f
+	var interrupted uint32
+
+	var counter uint32
+	for i := 0; i < limit; i++ {
+		wg.Add(1)
 		go func() {
 			defer wg.Done()
+
+			var index int // 第几个函数
+
 			defer func() {
 				r := recover()
-				recovereds[i] = r
+				if r != nil {
+					atomic.SwapUint32(&interrupted, 1)
+					recovereds[index] = r
+				}
 			}()
 
-			err := f()
-			errs[i] = err
+			for {
+				if atomic.LoadUint32(&interrupted) != 0 {
+					// 前面的函数错误或者panic
+					// 不再执行后面的函数
+					break
+				}
+
+				index = int(atomic.AddUint32(&counter, 1) - 1)
+				if index >= len(funcs) {
+					break
+				}
+
+				err := funcs[index]()
+				if err != nil {
+					atomic.SwapUint32(&interrupted, 1)
+					errs[index] = err
+				}
+			}
 		}()
 	}
+
 	wg.Wait()
 
 	for _, r := range recovereds {
--- async/parallel_test.go
+++ async/parallel_test.go
@@ -3,6 +3,7 @@ package async
 import (
 	"errors"
 	"testing"
+	"time"
 )
 
 func TestParallel(t *testing.T) {
@@ -26,6 +27,38 @@ func TestParallel(t *testing.T) {
 			},
 		},
 		{
+			name: "two",
+			args: args{
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 10)
+						return nil
+					},
+				},
+			},
+		},
+		{
+			name: "three",
+			args: args{
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 10)
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 20)
+						return nil
+					},
+				},
+			},
+		},
+		{
 			name: "error",
 			args: args{
 				funcs: []func() error{
@@ -36,11 +69,38 @@ func TestParallel(t *testing.T) {
 			},
 			wantErr: true,
 		},
-
+		{
+			name: "partial_error",
+			args: args{
+				funcs: []func() error{
+					func() error {
+						return errors.New("wow")
+					},
+					func() error {
+						return nil
+					},
+				},
+			},
+			wantErr: true,
+		},
 		{
 			name: "panic",
 			args: args{
 				funcs: []func() error{
+					func() error {
+						panic("wow")
+					},
+				},
+			},
+			wantPanic: true,
+		},
+		{
+			name: "partial_panic",
+			args: args{
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
 					func() error {
 						panic("wow")
 					},
@@ -60,6 +120,141 @@ func TestParallel(t *testing.T) {
 				}()
 
 				if err := Parallel(tt.args.funcs...); (err != nil) != tt.wantErr {
+					t.Errorf("Parallel() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			}()
+		})
+	}
+}
+
+func TestParallelLimit(t *testing.T) {
+	type args struct {
+		limit int
+		funcs []func() error
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantPanic bool
+		wantErr   bool
+	}{
+		{
+			name: "ok",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+				},
+			},
+		},
+		{
+			name: "two",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 10)
+						return nil
+					},
+				},
+			},
+		},
+		{
+			name: "three",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 10)
+						return nil
+					},
+					func() error {
+						time.Sleep(time.Millisecond * 20)
+						return nil
+					},
+				},
+			},
+		},
+		{
+			name: "error",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return errors.New("wow")
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "partial_error",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return errors.New("wow")
+					},
+					func() error {
+						return nil
+					},
+					func() error {
+						return errors.New("wow")
+					},
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "panic",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						panic("wow")
+					},
+					func() error {
+						panic("wow")
+					},
+				},
+			},
+			wantPanic: true,
+		},
+		{
+			name: "partial_panic",
+			args: args{
+				limit: 2,
+				funcs: []func() error{
+					func() error {
+						return nil
+					},
+					func() error {
+						panic("wow")
+					},
+				},
+			},
+			wantPanic: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			func() {
+				defer func() {
+					r := recover()
+					if r == nil && tt.wantPanic {
+						t.Errorf("recover() = %v, want panic: %t", r, tt.wantPanic)
+					}
+				}()
+
+				if err := ParallelLimit(tt.args.limit, tt.args.funcs...); (err != nil) != tt.wantErr {
 					t.Errorf("Parallel() error = %v, wantErr %v", err, tt.wantErr)
 				}
 			}()
