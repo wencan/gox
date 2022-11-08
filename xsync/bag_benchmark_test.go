f41bf9bcacf486198a427183218083ddc3334e8e
wencan <i@wencan.org>
2022-11-08 13:03:12 +0800

    update benchmark test

--- xsync/bag_benchmark_test.go
+++ xsync/bag_benchmark_test.go
@@ -44,3 +44,35 @@ func BenchmarkSyncMapWrite(b *testing.B) {
 		}
 	})
 }
+
+func BenchmarkBagRange(b *testing.B) {
+	bag := NewBag()
+
+	for i := 0; i < 10000; i++ {
+		bag.Add(i)
+	}
+
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			bag.Range(func(p interface{}) (stopIteration bool) {
+				return false
+			})
+		}
+	})
+}
+
+func BenchmarkSyncMapRange(b *testing.B) {
+	var mapping sync.Map
+
+	for i := 0; i < 10000; i++ {
+		mapping.Store(i, i)
+	}
+
+	b.RunParallel(func(p *testing.PB) {
+		for p.Next() {
+			mapping.Range(func(key, value any) bool {
+				return true
+			})
+		}
+	})
+}
